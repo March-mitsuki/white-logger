9a5eb22a97762f567e26ceefbbc2836a44d6460c
March-mitsuki <march_tsuki@icloud.com>
2022-12-13 09:24:38 +0900

    [add] init enviromention

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: "./",
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+  },
+  ignorePatterns: [".eslintrc.*"],
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function logger() {
+  console.log("hello, world!");
+}
+
+logger();
